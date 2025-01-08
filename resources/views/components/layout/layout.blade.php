3075353152203f907e032e2a36afd25d6627a7d0
sediqullah ekhtyar <sediqullah72910@gmail.com>
2025-01-08 14:50:51 +0430

    fullscreen feature implemented

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -17,7 +17,58 @@
 
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <style>
+        /* Style the restore prompt */
+        #restoreFullscreenPrompt {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: rgba(0, 0, 0, 0.8);
+            color: white;
+            padding: 20px;
+            border-radius: 10px;
+            z-index: 9999;
+            text-align: center;
+            width: 300px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        }
 
+        /* General button styles */
+        .prompt-button {
+            padding: 10px 20px;
+            margin: 10px;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+            transition: all 0.3s ease;
+        }
+
+        /* Style for "Yes" button */
+        .yes-button {
+            background-color: #28a745;
+            /* Green */
+            color: white;
+        }
+
+        .yes-button:hover {
+            background-color: #218838;
+            /* Darker green */
+        }
+
+        /* Style for "No" button */
+        .no-button {
+            background-color: #dc3545;
+            /* Red */
+            color: white;
+        }
+
+        .no-button:hover {
+            background-color: #c82333;
+            /* Darker red */
+        }
+    </style>
 
 </head>
 
@@ -31,6 +82,9 @@
                 <div class="relative">
                     <img src="/storage/system_images/logo.webp" class="h-10" alt="Logo">
                 </div>
+                <button id="fullscreenButton" class="text-gray-500 hover:text-gray-800">
+                    <i class="fa fa-expand" aria-hidden="true"></i>
+                </button>
                 <h1 class="text-xl text-white font-bold sm:hidden">LMIS</h1>
             </div>
             <h1 class="text-xl text-white font-bold hidden sm:block">سیستم مدیریت جواز ها</h1>
@@ -54,7 +108,7 @@
 
     <!-- Sidebar -->
     <aside id="sidebar"
-        class="fixed top-0 right-0 z-40 w-68 h-full pt-16 transition-transform translate-x-full bg-[#2C3E50] border-l border-gray-200 sm:translate-x-0"
+        class="fixed top-0 right-0 z-40 w-20 h-full pt-16 transition-transform translate-x-full bg-[#2C3E50] border-l border-gray-200 sm:translate-x-0"
         dir="rtl">
 
         <ul class="space-y-2 p-2">
@@ -62,7 +116,7 @@
                 <a href="{{ route('dashboard') }}"
                     class="flex items-center p-2 hover:bg-[#D4AF37] hover:text-gray-900 rounded-lg {{ request()->routeIs('dashboard') ? 'text-gray-900' : 'text-white' }}">
                     <i class="fa fa-home"></i>
-                    <p class="mr-3">داشبورد سیستم</p>
+                    <p class="mr-3 hidden">داشبورد سیستم</p>
 
                 </a>
             </li>
@@ -70,7 +124,7 @@
                 <a href="#" @click.prevent="open = !open"
                     class="flex items-center p-2 text-sm hover:bg-[#D4AF37] hover:text-gray-900 rounded-lg {{ request()->routeIs('individuals') || request()->routeIs('companies') ? '!bg-[#D4AF37] text-gray-900' : 'text-white' }}">
                     <i class="fa-solid fa-address-card ml-2"></i>
-                    <p class="">متقاضیان</p>
+                    <p class="hidden">متقاضیان</p>
                     <i class="mr-2 mt-1 fa {{ !(request()->routeIs('individuals') || request()->routeIs('companies')) ? 'text-gray-400' : '' }} "
                         :class="open ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
                 </a>
@@ -81,7 +135,7 @@
                         <a href="{{ route('individuals') }}"
                             class="flex items-center p-2 text-sm hover:bg-[#D4AF37] hover:text-gray-900 rounded-lg {{ request()->routeIs('individuals') ? 'text-gray-900' : 'text-white' }}">
                             <i class="fa fa-user"></i>
-                            <p class="mr-3">اشخاص</p>
+                            <p class="mr-3 hidden">اشخاص</p>
                         </a>
                     </li>
                     <li
@@ -89,7 +143,7 @@
                         <a href="{{ route('companies') }}"
                             class="flex items-center p-2 text-sm hover:bg-[#D4AF37] hover:text-gray-900 rounded-lg {{ request()->routeIs('companies') ? 'text-gray-900' : 'text-white' }}">
                             <i class="fa fa-building"></i>
-                            <p class="mr-3">شرکت ها</p>
+                            <p class="mr-3 hidden">شرکت ها</p>
                         </a>
                     </li>
                 </ul>
@@ -99,7 +153,7 @@
                 {{-- href="http://172.20.3.17:3030" target="_blank" rel="noopener noreferrer" --}}
                 <a class="flex items-center p-2 text-white text-sm hover:bg-[#D4AF37] hover:text-gray-900 rounded-lg">
                     <i class="fa fa-mountain"></i>
-                    <p class="mr-3"> جواز های حرفه یی</p>
+                    <p class="mr-3 hidden"> جواز های حرفه یی</p>
                 </a>
             </li>
 
@@ -113,7 +167,7 @@
                         <path
                             d="M208 64a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zM9.8 214.8c5.1-12.2 19.1-18 31.4-12.9L60.7 210l22.9-38.1C99.9 144.6 129.3 128 161 128c51.4 0 97 32.9 113.3 81.7l34.6 103.7 79.3 33.1 34.2-45.6c6.4-8.5 16.6-13.3 27.2-12.8s20.3 6.4 25.8 15.5l96 160c5.9 9.9 6.1 22.2 .4 32.2s-16.3 16.2-27.8 16.2l-256 0c-11.1 0-21.4-5.7-27.2-15.2s-6.4-21.2-1.4-31.1l16-32c5.4-10.8 16.5-17.7 28.6-17.7l32 0 22.5-30L22.8 246.2c-12.2-5.1-18-19.1-12.9-31.4zm82.8 91.8l112 48c11.8 5 19.4 16.6 19.4 29.4l0 96c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-74.9-60.6-26-37 111c-5.6 16.8-23.7 25.8-40.5 20.2S-3.9 486.6 1.6 469.9l48-144 11-33 32 13.7z" />
                     </svg>
-                    <p class="mr-3">جواز های مواد ساختمانی</p>
+                    <p class="mr-3 hidden">جواز های مواد ساختمانی</p>
                 </a>
             </li>
             <li class="group">
@@ -125,7 +179,7 @@
                             d="m17,11.5v-6.304l-7,5.804v8h-2V2h1V0H0v2h1v19c0,1.654,1.346,3,3,3h17c1.654,0,3-1.346,3-3V5.196l-7,6.304Zm-1,7.5h-3v-3h3v3Zm5,0h-3v-3h3v3Z" />
                     </svg>
 
-                    <p class="mr-3">جواز پروسس برای فابریکه ها</p>
+                    <p class="mr-3 hidden">جواز پروسس برای فابریکه ها</p>
                 </a>
             </li>
             <li
@@ -133,14 +187,14 @@
                 <a href="{{ route('activity_logs') }}"
                     class="flex items-center p-2 hover:bg-[#D4AF37] hover:text-gray-900 rounded-lg {{ request()->routeIs('activity_logs') ? 'text-gray-900' : 'text-white' }}">
                     <i class="fas fa-clipboard-list"></i>
-                    <p class="mr-3">فعالیت ها</p>
+                    <p class="mr-3 hidden">فعالیت ها</p>
                 </a>
             </li>
         </ul>
     </aside>
 
     <!-- Content -->
-    <main id="content" class="pt-16 lg:mr-64 transition-all">
+    <main id="content" class="pt-16 lg:mr-20 transition-all">
         {{ $slot }}
     </main>
 
@@ -207,8 +261,72 @@
             }
         });
     </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const button = document.getElementById('fullscreenButton');
 
+            const isFullscreen = localStorage.getItem('isFullscreen') === 'true';
 
+            if (document.fullscreenElement || isFullscreen) {
+                button.innerHTML = '<i class="fa fa-compress" aria-hidden="true"></i>';
+            } else {
+                button.innerHTML = '<i class="fa fa-expand" aria-hidden="true"></i>';
+            }
+
+            if (isFullscreen && !document.fullscreenElement) {
+                const restorePrompt = document.createElement('div');
+                restorePrompt.id = 'restoreFullscreenPrompt';
+                restorePrompt.innerHTML = `
+            <p>آیا میخواهد دوباره صفحه را بزرگ نمایی کنید؟</p>
+            <button id="restoreFullscreenButton" class="prompt-button yes-button">بلی</button>
+            <button id="cancelRestoreButton" class="prompt-button no-button">خیر</button>
+        `;
+                document.body.appendChild(restorePrompt);
+
+                document.getElementById('restoreFullscreenButton').addEventListener('click', () => {
+                    document.documentElement.requestFullscreen().then(() => {
+                        localStorage.setItem('isFullscreen', 'true');
+                        document.body.removeChild(restorePrompt);
+                    }).catch(err => {
+                        console.error(`Failed to restore fullscreen: ${err.message}`);
+                    });
+                });
+
+                document.getElementById('cancelRestoreButton').addEventListener('click', () => {
+                    document.body.removeChild(restorePrompt);
+                    localStorage.setItem('isFullscreen', 'false');
+                });
+            }
+
+            button.addEventListener('click', () => {
+                if (!document.fullscreenElement) {
+                    document.documentElement.requestFullscreen().then(() => {
+                        button.innerHTML = '<i class="fa fa-compress" aria-hidden="true"></i>';
+                        localStorage.setItem('isFullscreen', 'true');
+                    }).catch(err => {
+                        console.error(`Error entering fullscreen: ${err.message}`);
+                    });
+                } else {
+                    document.exitFullscreen().then(() => {
+                        button.innerHTML = '<i class="fa fa-expand" aria-hidden="true"></i>';
+                        localStorage.setItem('isFullscreen', 'false');
+                    }).catch(err => {
+                        console.error(`Error exiting fullscreen: ${err.message}`);
+                    });
+                }
+            });
+
+            document.addEventListener('fullscreenchange', () => {
+                if (!document.fullscreenElement) {
+                    button.innerHTML = '<i class="fa fa-expand" aria-hidden="true"></i>';
+                    localStorage.setItem('isFullscreen', 'false');
+                } else {
+                    button.innerHTML = '<i class="fa fa-compress" aria-hidden="true"></i>';
+                    localStorage.setItem('isFullscreen', 'true');
+                }
+            });
+        });
+    </script>
 
 </body>
 
