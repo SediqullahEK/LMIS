e5708814ab332ef7d2617315383f26eef7c9ab71
unknown <sediqullah72910@gmail.com>
2025-01-09 12:51:43 +0430

    precious semi precious component has been created

---
+++ app/Livewire/PreciousStonesLicenses/PreciousStonesLicenses.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\PreciousStonesLicenses;
+
+use Livewire\Component;
+
+class PreciousStonesLicenses extends Component
+{
+    public function render()
+    {
+        return view('livewire.PreciousStonesLicenses.precious-stones-licenses');
+    }
+}
---
+++ resources/views/pages/PreciousStonesLicenses/preciousStonesLicenses.blade.php
@@ -0,0 +1,3 @@
+<x-layout.layout>
+    <livewire:PreciousStonesLicenses.precious-stones-licenses />
+</x-layout.layout>
